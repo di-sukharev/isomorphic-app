8934646f0411353d35df70d9d952545034cafe63
Dima <dima@sukharev.io>
2020-05-11 18:57:11 +0300

    added Search.js unit test

--- src/shared/components/search.test.js
+++ src/shared/components/search.test.js
@@ -1,18 +1,45 @@
 /* eslint-disable camelcase */
 import React from "react";
-import ShallowRenderer from "react-test-renderer/shallow";
+import renderer from "react-test-renderer";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import Search from "./Search";
+import "@testing-library/jest-dom/extend-expect";
 
 import PLACES from "./__mocks__/places";
 import DATES from "./__mocks__/date";
 
-const shallow = new ShallowRenderer();
-
-describe("Info tests", () => {
-  it("renders correctly", () => {
-    shallow.render(
+describe("Snapshots", () => {
+  it("Renders correctly", () => {
+    const component = renderer.create(
       <Search places={PLACES} dates={DATES} onPlaceChange={() => {}} onDateChange={() => {}} />
     );
-    expect(shallow.getRenderOutput()).toMatchSnapshot();
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("Changes autocomplete options on input", async () => {
+    const { getByRole, getAllByText } = render(
+      <Search places={PLACES} dates={DATES} onPlaceChange={() => {}} onDateChange={() => {}} />
+    );
+
+    const query = "Am"; // "Am" matches -> Amsterdam, Rotterdam, ...
+    const filteredPlaces = PLACES.filter(place =>
+      place.place_name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    fireEvent.focus(getByRole("combobox")); // simulate focus event on input
+    fireEvent.change(getByRole("combobox"), { target: { value: query } }); // simulate change event on input — value: Utrecht
+    const renderedPlaces = await waitFor(() =>
+      getAllByText(query, {
+        exact: false,
+        selector: ".ant-select-item-option-content",
+      })
+    ); // take rendered renderedPlaces — ant-select-item-option-content
+
+    expect(renderedPlaces.length).toEqual(filteredPlaces.length);
+
+    // filteredPlaces equals to renderedPlaces
+    for (let i = 0; i < filteredPlaces.length; i++) {
+      expect(renderedPlaces[i]).toHaveTextContent(filteredPlaces[i].place_name);
+    }
   });
 });
