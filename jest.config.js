fa815380626904cb252efa1aa3bce50ae1d40e44
Dima <dima@sukharev.io>
2020-05-11 16:25:09 +0300

    jest full setup

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
+  setupFilesAfterEnv: ["<rootDir>jest.setup.js"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  testEnvironment: "jsdom",
 };
---
+++ jest.setup.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+Object.defineProperty(window, "matchMedia", {
+  value: jest.fn(() => ({
+    matches: true,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  })),
+});
