8da063d28d951398a2c2b26a720f5c454ee61843
Dima <dima@sukharev.io>
2020-05-12 20:16:51 +0300

    added unit tests

--- src/shared/components/info.test.js
+++ src/shared/components/info.test.js
@@ -2,7 +2,15 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { render } from "@testing-library/react";
 import Info from "./Info";
-import PLACES from "./__mocks__/places";
-import DATES from "./__mocks__/dates";
 
-describe("<Info/>", () => {});
+// todo: take FAKES from /tools/weatherFabric.js
+import FAKE_PLACES from "./__mocks__/places";
+import FAKE_DATES from "./__mocks__/dates";
+
+describe("Unit test of <Info/> component", () => {
+  it("renders place name and date", () => {
+    const { getByText } = render(<Info place={FAKE_PLACES[0]} date={FAKE_DATES[0]} />);
+    expect(getByText(FAKE_PLACES[0].place_name)).toBeInTheDocument();
+    expect(getByText(FAKE_DATES[0])).toBeInTheDocument();
+  });
+});
--- src/shared/components/search.test.js
+++ src/shared/components/search.test.js
@@ -1,10 +1,81 @@
 /* eslint-disable camelcase */
 import React from "react";
 import renderer from "react-test-renderer";
-import { render, fireEvent, waitFor } from "@testing-library/react";
+import { render, fireEvent, waitFor, cleanup } from "@testing-library/react";
 import Search from "./Search";
 
-import PLACES from "./__mocks__/places";
-import DATES from "./__mocks__/dates";
+import data from "../../../data.json";
 
-describe("<Search/>", () => {});
+// ======== converting "data.json" to needed "weather" structure ========
+const weather = {};
+
+data.forEach(v => {
+  const [date] = v.datetime.split(" ");
+  if (weather[date]) weather[date].push(v);
+  else weather[date] = [v];
+});
+
+const dateKeys = Object.keys(weather); // string[]
+const defaultDate = dateKeys[0]; // string
+const defaultPlace = weather[defaultDate][0]; // {}
+// ================
+
+const FAKE_DATES = { selected: defaultDate, all: dateKeys };
+const FAKE_PLACES = { selected: defaultPlace, all: weather };
+
+describe("Kind of integration (not really) test of <AutoComplete/> and <DatePicker/> in <Search/> together", () => {
+  afterEach(cleanup);
+
+  it("takes callbacks and calls them with values when selected", async () => {
+    let placeChangeCalledTimes = 0;
+    let dateChangeCalledTimes = 0;
+    const onPlaceChange = jest.fn();
+    const onDateChange = jest.fn();
+    const { getByText, getByRole, getByPlaceholderText, getByTitle } = render(
+      <Search
+        places={FAKE_PLACES}
+        dates={FAKE_DATES}
+        onPlaceChange={onPlaceChange}
+        onDateChange={onDateChange}
+      />
+    );
+
+    const testPlace = async place => {
+      fireEvent.change(getByRole(/combobox/i), { target: { value: place } });
+      let option = null;
+      await waitFor(() => {
+        option = getByText(place, {
+          ignore: "[role=option]",
+        });
+      });
+
+      fireEvent.click(option);
+      expect(onPlaceChange).toHaveBeenCalledWith(place, { value: place });
+      expect(onPlaceChange).toHaveBeenCalledTimes(++placeChangeCalledTimes);
+    };
+
+    const testDate = async date => {
+      fireEvent.click(getByPlaceholderText(/Enter date/i));
+
+      let option = null;
+      await waitFor(() => {
+        option = getByTitle(date);
+      });
+
+      fireEvent.click(option); // todo: CLICK DOESN'T WORK
+      expect(onDateChange).toHaveBeenCalledWith(date, { value: date });
+      expect(onDateChange).toHaveBeenCalledTimes(++dateChangeCalledTimes);
+    };
+
+    // testing autocomplete
+    await testPlace("Maastricht");
+    await testPlace("Amsterdam");
+    await testPlace("Enschede");
+
+    // testing date picker
+    await testDate("2014-08-08");
+    await testDate("2014-08-10");
+    await testDate("2014-08-11");
+    await testDate("2014-08-12");
+  });
+});
